ee04f935f2ddc15ad36c709ebe514ee3ed943ac1
ilekavic <watuuu@att.net>
2015-04-30 15:58:06 -0700

    week 4 assignment
    makefile and python program

---
+++ week_4/Hwk4.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 29 09:28:57 2015
+
+@author: IL
+"""
+
+with open('C:/Users/IL/Documents/SciComp/HWK4/hello.f90') as file:
+    #data = file.read().replace('/n', '')
+    #data = [[str(digit) for digit in line.split()] for line in file]
+    data = file.read()
+
+print data
